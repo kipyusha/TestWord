f92664bcaf3252754895c8c98626cae27246a953
kipyusha <107037736+kipyusha@users.noreply.github.com>
2024-02-12 20:31:24 +0600

    Add files via upload

---
+++ client/app/(site)/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Home() {
+  return (
+    <main className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
+      <div>main</div>
+    </main>
+  );
+}
